3d3e057c4b98cd4374183f1f175077a9af0fd94e
karishmask46 <karishmask.15999@gmail.com>
2022-07-21 20:39:29 +0530

    Data structures using linked list

---
+++ DataStructures/DataStructures/Program.cs
@@ -0,0 +1,13 @@
+﻿using DataStructures;
+Console.WriteLine("Hello World!");
+LinkedList list = new LinkedList();
+list.Add(56);
+list.Add(30);
+list.Add(70);
+list.AddAll(23, 56, 78, 34, 56);
+list.InsertAtParticularPosition(2, 30);
+list.RemoveFirstNode();
+list.Search(56);
+list.RemovaLastNode();
+list.Display();
+
