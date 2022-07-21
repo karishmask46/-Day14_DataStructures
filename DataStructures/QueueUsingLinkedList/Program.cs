fe053f249127131c5077ded25e0e9fc719d63727
karishmask46 <karishmask.15999@gmail.com>
2022-07-21 20:55:19 +0530

    Queue using Linked List

---
+++ DataStructures/QueueUsingLinkedList/Program.cs
@@ -0,0 +1,7 @@
+﻿using QueueUsingLinkedList;
+Console.WriteLine("Hello World!");
+LinkedListQueue linkedListQueue = new LinkedListQueue();
+linkedListQueue.Enqueue(56);
+linkedListQueue.Enqueue(30);
+linkedListQueue.Enqueue(70);
+linkedListQueue.Display();
