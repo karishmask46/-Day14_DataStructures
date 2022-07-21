be9117a291b6c6762e760ce48a1e29b8454f7881
karishmask46 <karishmask.15999@gmail.com>
2022-07-21 20:48:20 +0530

    Stack using Linked List

---
+++ DataStructures/StackUsingLinkedList/Program.cs
@@ -0,0 +1,11 @@
+﻿using StackUsingLinkedList;
+Console.WriteLine("Hello World!");
+LinkedListStack linkedListStack = new LinkedListStack();
+linkedListStack.Push(70);
+linkedListStack.Push(30);
+linkedListStack.Push(56);
+linkedListStack.Display();
+linkedListStack.Peek();
+linkedListStack.Pop();
+linkedListStack.IsEmpty();
+linkedListStack.Display();
